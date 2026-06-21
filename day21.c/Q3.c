537a6377d13db49895c63c5b53489e1be6dba52e
samya321 <samyasarim@gmail.com>
2026-06-21 19:59:39 +0530

    Update Q3.c

--- day21.c/Q3.c
+++ day21.c/Q3.c
@@ -4,7 +4,7 @@ int main() {
     char str[100];
     int countv=0,countc=0,i=0;
     printf("enter the string:\n");
-    scanf("%s",&str);
+    scanf("%s",str);
     while(str[i]!='\0'){
        if(str[i]=='a'|| str[i]=='A'||str[i]=='e'||str[i]=='E'||str[i]=='i'||str[i]=='I'||str[i]=='o'||str[i]=='O'||str[i]=='u'||str[i]=='U'){
          countv++;
